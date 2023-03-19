8e61030252130cea73fea6810ce60deec3ca104a
gary <garyhe.05@outlook.com>
2023-03-19 21:50:41 +0800

    first commit

---
+++ example.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @param {*} a  is a number
+ * @param {*} b  is a number
+ * @returns sum of a and b
+ */
+
+function add(a, b) {
+	return Number(a) + Number(b)
+}
+
+
+export { add }
+
---
+++ example.test.js
@@ -0,0 +1,7 @@
+import { add } from './example';
+
+
+test('1 + 2 equals 3', () => {
+	expect(add(1, 2)).toBe(3);
+})
+
